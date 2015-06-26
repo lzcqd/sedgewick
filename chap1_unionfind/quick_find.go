15de982302fb2df5fd4c171791f3c82a2d9d170b
Li Zichen <lizichen.qd@gmail.com>
2015-06-26 21:52:16 +0800

    Quick-find: Implement tests

---
+++ chap1_unionfind/quick_find.go
@@ -0,0 +1,46 @@
+/**********************************
+/ Sedgewick's algorithm edition 4
+/ Chapter 1 Quick Find
+*********************************/
+package chap1_unionfind
+
+type Sites struct {
+	id     []int
+	number int
+}
+
+func Init(n int) *Sites {
+	sites := &Sites{make([]int, n), n}
+	for i := range sites.id {
+		sites.id[i] = i
+	}
+	return sites
+}
+
+func (s *Sites) Union(p, q int) {
+	pId := s.Find(p)
+	qId := s.Find(q)
+
+	if pId == qId {
+		return
+	}
+
+	for i := range s.id {
+		if s.Find(i) == pId {
+			s.id[i] = qId
+		}
+	}
+	s.number = s.number - 1
+}
+
+func (s Sites) Find(p int) int {
+	return s.id[p]
+}
+
+func (s Sites) Connected(p, q int) bool {
+	return s.Find(p) == s.Find(q)
+}
+
+func (s Sites) Count() int {
+	return s.number
+}
