3a1bd8292c798b89f308dc027a4f5d8c1077cc47
Li Zichen <lizichen.qd@gmail.com>
2015-07-09 00:00:12 +0800

    Sortable: add Copyable interface

--- chap2_sorting/sortable/sortable.go
+++ chap2_sorting/sortable/sortable.go
@@ -14,27 +14,57 @@ type Interface interface {
 
 type Intslice []int
 
-func (a Intslice) Len() int                   { return len(a) }
-func (a Intslice) Swap(i, j int)              { a[i], a[j] = a[j], a[i] }
-func (a Intslice) Less(i, j int) bool         { return a[i] < a[j] }
-func (a Intslice) Get(i int) interface{}      { return nil }
-func (a Intslice) Set(i int, val interface{}) {}
+func (a Intslice) Len() int           { return len(a) }
+func (a Intslice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Intslice) Less(i, j int) bool { return a[i] < a[j] }
 
 type Stringslice []string
 
-func (s Stringslice) Len() int                   { return len(s) }
-func (s Stringslice) Swap(i, j int)              { s[i], s[j] = s[j], s[i] }
-func (s Stringslice) Less(i, j int) bool         { return s[i] < s[j] }
-func (s Stringslice) Get(i int) interface{}      { return nil }
-func (s Stringslice) Set(i int, val interface{}) {}
+func (s Stringslice) Len() int           { return len(s) }
+func (s Stringslice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Stringslice) Less(i, j int) bool { return s[i] < s[j] }
 
 type Floatslice []float64
 
-func (a Floatslice) Len() int              { return len(a) }
-func (a Floatslice) Swap(i, j int)         { a[i], a[j] = a[j], a[i] }
-func (a Floatslice) Less(i, j int) bool    { return a[i] < a[j] }
-func (a Floatslice) Get(i int) interface{} { return a[i] }
-func (a Floatslice) Set(i int, val interface{}) {
+func (a Floatslice) Len() int           { return len(a) }
+func (a Floatslice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Floatslice) Less(i, j int) bool { return a[i] < a[j] }
+
+type Copyable interface {
+	Init(count int)
+	Get(i int) interface{}
+	Set(i int, val interface{})
+}
+
+type CopyableIntslice []int
+
+func (a *CopyableIntslice) Init(count int) {
+	*a = make([]int, count)
+}
+func (a CopyableIntslice) Get(i int) interface{} { return a[i] }
+func (a CopyableIntslice) Set(i int, val interface{}) {
+	v := reflect.ValueOf(val)
+	a[i] = int(v.Int())
+}
+
+type CopyableStringslice []string
+
+func (s *CopyableStringslice) Init(count int) {
+	*s = make([]string, count)
+}
+func (s CopyableStringslice) Get(i int) interface{} { return s[i] }
+func (s CopyableStringslice) Set(i int, val interface{}) {
+	v := reflect.ValueOf(val)
+	s[i] = v.String()
+}
+
+type CopyableFloatslice []float64
+
+func (a *CopyableFloatslice) Init(count int) {
+	*a = make([]float64, count)
+}
+func (a CopyableFloatslice) Get(i int) interface{} { return a[i] }
+func (a CopyableFloatslice) Set(i int, val interface{}) {
 	v := reflect.ValueOf(val)
 	a[i] = v.Float()
 }
