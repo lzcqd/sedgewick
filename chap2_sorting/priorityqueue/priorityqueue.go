4ef96172083eb370dcca278088c8736d8ea8508b
Li Zichen <lizichen.qd@gmail.com>
2015-07-17 11:17:54 +0800

    int priorityqueue implementation

--- chap2_sorting/priorityqueue/priorityqueue.go
+++ chap2_sorting/priorityqueue/priorityqueue.go
@@ -34,10 +34,15 @@ func (i IntPriorityQueue) Insert(key interface{}) {
 	k := myint(int(v.Int()))
 
 	i = append(i, k)
+	swim(i.toComparable(), len(i)-1)
 }
 
 func (i IntPriorityQueue) DelMax() interface{} {
-
+	r := i[0]
+	i[0], i[len(i)-1] = i[len(i)-1], i[0]
+	i = i[:len(i)-1]
+	sink(i.toComparable(), 0, len(i))
+	return r
 }
 
 func (i IntPriorityQueue) toComparable() []comparable {
@@ -50,10 +55,23 @@ func (i IntPriorityQueue) toComparable() []comparable {
 	return c
 }
 
-func swim(array []comparable, curr, total int) {
-
+func swim(array []comparable, curr int) {
+	for curr > 0 && array[curr].compare((curr-1)/2) > 0 {
+		array[curr], array[(curr-1)/2] = array[(curr-1)/2], array[curr]
+		curr = (curr - 1) / 2
+	}
 }
 
 func sink(array []comparable, curr, total int) {
-
+	for curr*2+1 <= total {
+		c := curr*2 + 1
+		if c < total && array[c].compare(c+1) < 0 {
+			c = c + 1
+		}
+		if array[curr].compare(c) > 0 {
+			break
+		}
+		array[curr], array[c] = array[c], array[curr]
+		curr = c
+	}
 }
